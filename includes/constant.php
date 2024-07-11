32d229efca7114ed142189689342ce07b565e15d
vinay sachan <vinay@tappetbox.com>
2024-07-11 21:46:58 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -6,7 +6,7 @@ define('CONST_MAIL_PASSWORD', 'EmailPassword@123');
 define('CONST_MAIL_FROM', 'contact@livledger.com');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'Crypto');
-//define('CONST_RECEIVE_EMAIL', 'james_smith.91@outlook.com');
-define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
+define('CONST_RECEIVE_EMAIL', 'james_smith.91@outlook.com');
+//define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
